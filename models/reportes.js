c44bca691c474dc024c9f30803064d3997300be2
Unknown <dh.mahecha@uniandes.edu.co>
2018-03-15 21:07:13 -0500

    Taller 6 - Herramienta de automatización

---
+++ models/reportes.js
@@ -0,0 +1,15 @@
+// app/models/reportes.js
+
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ReportesSchema   = new Schema({
+    ruta_img_base: String,
+    ruta_img_modificada: String,
+    ruta_img_salida: String,	
+    diferencias: String,
+    informacion: String,
+    fecha: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Reportes', ReportesSchema);
